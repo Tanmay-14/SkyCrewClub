d937e8f1749ccc0475398bfc6676c824caf6b139
Varad Gundap <varad615@gmail.com>
2022-06-13 23:52:14 +0530

    404

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,24 @@
 import styles from "../styles/NotFound.module.css";
-
+import Notfound from "../public/404.png";
+import Image from "next/image";
+import Link from "next/link";
 export default function Custom404() {
   return (
     <div>
       <center>
-
         <div className={styles.card}>
-            <h1>404</h1>
-        
-            <h2>Page not found</h2>
-            
-            <div className={styles.container}>
-              
-              <p>I guess you are lost. Still you can go back.</p> 
-          
-            </div>
+          <div className={styles.img}>
+            <Image src={Notfound} alt="404" width={300} height={300} />
+          </div>
+          <h1>404</h1>
+          <h2>Page not found</h2>
+          <div className={styles.container}>
+            I guess you are lost. Still you can go back.
+            <p />
+            <Link href="/">
+              <button>Home</button>
+            </Link>
+          </div>
         </div>
       </center>
     </div>
