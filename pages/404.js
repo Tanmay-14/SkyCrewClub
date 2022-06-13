cfcd00df6789fe4f673f2abdf98c3518237469f5
Kalyani <kalyani.bhagat18@gmail.com>
2022-06-13 22:36:02 +0530

    404 updated

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,21 @@
+import styles from "../styles/NotFound.module.css";
+
 export default function Custom404() {
   return (
     <div>
       <center>
-        <h1>404</h1>
-        <h2>Page not found</h2>
 
-        <p />
-        <h3> I guess you are lost. Still you can go back.</h3>
+        <div className={styles.card}>
+            <h1>404</h1>
+        
+            <h2>Page not found</h2>
+            
+            <div className={styles.container}>
+              
+              <p>I guess you are lost. Still you can go back.</p> 
+          
+            </div>
+        </div>
       </center>
     </div>
   );
