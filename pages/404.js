0f718f647f9d463ce2c7a13ca65411589bb09264
Varad Gundap <varad615@gmail.com>
2022-06-13 15:09:44 +0530

    bug fixes

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,13 @@
-import styles from "../styles/Nopage.module.css";
-import Image from 'next/image'
-import Nopage from '../public/404.png'
-
 export default function Custom404() {
   return (
     <div>
-      <div className={styles.body}>
-        <Image src={Nopage} height={450} width={600}/>
-        
-      </div>
+      <center>
+        <h1>404</h1>
+        <h2>Page not found</h2>
+
+        <p />
+        <h3> I guess you are lost. Still you can go back.</h3>
+      </center>
     </div>
   );
 }
